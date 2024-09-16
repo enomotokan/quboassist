8739e15bb6a88631f02ce5c9b971d12795a120e5
unknown <enomotokan@gmail.com>
2024-09-16 15:12:17 +0900

    アルゴリズムと結果の可視化の実装完了

--- bandashi.py
+++ bandashi.py
@@ -2,7 +2,6 @@ from tytan import symbols, symbols_list, Compile
 import numpy as np
 import neal
 import optuna
-import random
 from matplotlib import pyplot as plt
 
 def A(n):
@@ -26,8 +25,8 @@ def add_qubo(qubo, key, var):
 S = 2000
 N = 36
 
-m = 40
-M = 60
+m = 0
+M = 70
 
 # 各看板の座標を生成
 
@@ -41,6 +40,8 @@ for i in range(6):
     for j in range(6):
         add = np.append(add, [[0.2 * i, 0.2 * j]], axis=0)
 
+# それぞれの営業マンが行くべき看板の番号
+Sn = []
 
 # for p in cod:
 #     plt.plot(p[0], p[1], marker=".", color="red")
@@ -53,12 +54,13 @@ for i in range(6):
 def dist(x, y):
     return float(np.sum(np.abs(x - y)))
 
-# 各営業マンが訪問可能な看板の番号
-
+# 営業マンが訪問可能な看板までの距離
 r = 0.3
 
+# 各営業マンが訪問可能な看板の番号
 visitable = []
 visitable_N = []
+
 for n in range(N):
     visitable.append([])
     for s in range(S):
@@ -80,6 +82,7 @@ print(np.min(visitable_S_N))
 
 # 第一段　クラスタリング
 
+
 def objective(trial):
 
     l = [0.001, [trial.suggest_float("l_1", 0, 10), trial.suggest_float("l_2", 0, 100)]]
@@ -93,25 +96,23 @@ def objective(trial):
     H = 0
 
     aux_1 = []
-    # aux_2 = []
+    aux_2 = []
 
     for n in range(N):
-        aux_coef = A(len(visitable[n]) - m)
-        aux_coef_len = len(aux_coef)
-        aux_1.append(symbols_list(aux_coef_len, "aux{}".format(n) + "_{}"))
-
-        H += l[1][0] * (m + sum(aux_coef[i] * aux_1[-1][i] for i in range(aux_coef_len)) - sum(x[n][i] for i in range(len(visitable[n]))))**2
-
-
-        # aux_coef = A(M - len(visitable[n]))
+        # aux_coef = A(len(visitable[n]) - m)
         # aux_coef_len = len(aux_coef)
-        # aux_2.append(symbols_list(aux_coef_len), "aux{}_{}".format(n))
+        # aux_1.append(symbols_list(aux_coef_len, "aux{}".format(n) + "_{}"))
 
-        # H += l[1][0] * (sum(x[n][s] for s in range(len(visitable[n]))) + sum(aux_coef[i] * aux_2[-1][i] for i in range(aux_coef_len)) - M)**2
+        # H += l[1][0] * (m + sum(aux_coef[i] * aux_1[-1][i] for i in range(aux_coef_len)) - sum(x[n][i] for i in range(len(visitable[n]))))**2
+
+
+        aux_coef = A(M)
+        aux_coef_len = len(aux_coef)
+        aux_2.append(symbols_list(aux_coef_len, "aux{}".format(n) + "_{}"))
+
+        H += l[1][0] * (sum(x[n][s] for s in range(len(visitable[n]))) + sum(aux_coef[i] * aux_2[-1][i] for i in range(aux_coef_len)) - M)**2
 
     for s in range(S):
-        # if s % 100 == 0:
-        #     print(s)
         H += l[1][1] * (sum(x[n][i] for n in range(N) for i in range(len(visitable[n])) if s == visitable[n][i]) - 1)**2
 
     qubo, offset = Compile(H).get_qubo()
@@ -128,7 +129,6 @@ def objective(trial):
                 add_qubo(qubo, ("x{}_{}".format(n, visitable[n][i]), "x{}_{}".format(n, visitable[n][i_])), l[0] * dist(cod[visitable[n][i]], cod[visitable[n][i_]]))
 
     solver = neal.SimulatedAnnealingSampler()
-    seed = random.randrange(2**20)
 
     x = solver.sample_qubo(Q=qubo).first.sample
 
@@ -142,11 +142,8 @@ def objective(trial):
         S_x = 0
         for s in visitable[n]:
             S_x += int(x["x{}_{}".format(n, s)])
-        if S_x < m:
+        if S_x > M:
             Error_num += 1
-    print(Error_num)
-
-    Error_num = 0
 
     # 2
 
@@ -162,6 +159,12 @@ def objective(trial):
             Error_num += 1
     
     if Error_num == 0:
+        global Sn
+        for n in range(N):
+            Sn.append([])
+            for s in visitable[n]:
+                if x["x{}_{}".format(n, s)] == 1:
+                    Sn[-1].append(s)
         trial.study.stop()
 
     return Error_num
@@ -170,3 +173,91 @@ study = optuna.create_study(direction="minimize")
 study.optimize(objective, n_trials=1000)
 
 #　第二段　各営業マンに対して巡回セールスマンを解く
+
+print("\n\n")
+
+# それぞれの営業マンが回る順番に看板の番号を並べる
+turn_n = []
+
+def objective(n, Sn_n, add_n, cod_n):
+    def objective_n(trial):
+        l = [0.001, [trial.suggest_float("l_1", 0, 100), trial.suggest_float("l_2", 0, 100)]]
+
+        N_n = len(cod_n)
+        x_tp = symbols_list((N_n, N_n), "x{}_{}")
+
+        H = 0
+
+        # 目的函数
+
+        for s in range(N_n):
+             H += l[0] * (dist(add_n, cod_n[s])  * (x_tp[0][s] + x_tp[N_n - 1][s]))
+
+        for t in range(1, N_n - 1):
+            for s in range(N_n):
+                for s_ in range(s + 1, N_n):
+                    H += l[1][0] * dist(cod_n[s], cod_n[s_]) * x_tp[t][s] * x_tp[t + 1][s_]
+                   
+        # 制約条件
+
+        for s in range(N_n):
+            H += l[1][0] * (sum(x_tp[t][s] for t in range(N_n)) - 1)**2
+
+        for t in range(N_n):
+            H += l[1][1] * (sum(x_tp[t][s] for s in range(N_n)) - 1)**2
+
+        qubo, offset = Compile(H).get_qubo()
+
+        print("Get qubo")
+
+        solver = neal.SimulatedAnnealingSampler()
+
+        x = solver.sample_qubo(Q=qubo).first.sample
+
+        # 制約条件を充たすか確認
+
+        Error_num = 0
+
+        for n in range(N_n):
+            if sum(x["x{}_{}".format(t, s)] for t in range(N_n)) != 1:
+                Error_num += 1
+        
+        for t in range(N_n):
+            if sum(x["x{}_{}".format(t, s)] for s in range(N_n)) != 1:
+                Error_num += 1
+        
+        if Error_num == 0:
+            global turn_n
+            for t in range(N_n):
+                for s in range(N_n):
+                    if x["x{}_{}".format(t, s)] == 1:
+                        turn_n.append(Sn_n[s])
+
+            trial.study.stop()
+        
+        return Error_num
+
+    return objective_n
+
+for n in range(N):
+    print("営業マン {}".format(n))
+
+    Sn_n = Sn[n]
+    add_n = add[n]
+    cod_n = cod[Sn_n]
+
+    study = optuna.create_study(direction="minimize")
+    study.optimize(objective(n, Sn_n, add_n, cod_n), n_trials=1000)
+
+print(turn_n)
+
+for n in range(N):
+    color = list(np.random.choice(range(256), 3))
+    
+    plt.plot(add[n], turn_n[0], color)
+    plt.plot(add[n], turn_n[len(turn_n) - 1], color)
+
+    for s in len(turn_n) - 1:
+        plt.plot(turn_n[s], turn_n[s + 1], color)
+
+plt.show()
